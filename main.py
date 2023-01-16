137f16b685ed22efa653aedcd642364c16c29af9
amirarsalan117 <122307820+amirarsalan117@users.noreply.github.com>
2023-01-16 19:43:16 +0000

    recived input from user

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ board = [['-','-','-'],['-','-','-'],['-','-','-',]]
 for i in range(3):
     print(board[i])
 
+user_input = input("where you want to put your mark(x,y)(splite your number with comma ,) ").split(',')
