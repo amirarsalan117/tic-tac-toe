7494980ae5aa5d9a87920c43ee6e12157d6c05b5
amirarsalan117 <122307820+amirarsalan117@users.noreply.github.com>
2023-01-16 19:47:01 +0000

    show user input on board

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-board = [['-','-','-'],['-','-','-'],['-','-','-',]]
-for i in range(3):
-    print(board[i])
+def show_board():
+    for i in range(3):
+        print(board[i])
 
-user_input = input("where you want to put your mark(x,y)(splite your number with comma ,) ").split(',')
+
+board = [['-','-','-'],['-','-','-'],['-','-','-',]]
+show_board()
+
+wh
+    user_input = input("where you want to put your mark(x,y)(splite your number with comma ,) ").split(',')
+
+    board[int(user_input[0])][int(user_input[1])] = 'O'
+    show_board()
