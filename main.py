d1b859c953521a4462625966a8365aba9e9bc02c
amirarsalan117 <122307820+amirarsalan117@users.noreply.github.com>
2023-01-16 19:54:08 +0000

    seprate playerone and player Two with X/O

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ def show_board():
 
 board = [['-','-','-'],['-','-','-'],['-','-','-',]]
 show_board()
-
+user_one = True
 is_game_on = True
 while is_game_on:
     user_input = input("where you want to put your mark(x,y)(splite your number with comma ,) ").split(',')
-    board[int(user_input[0])][int(user_input[1])] = 'O'
+    if user_one:
+        board[int(user_input[0])][int(user_input[1])] = 'O'
+        user_one = False
+    else:
+        board[int(user_input[0])][int(user_input[1])] = 'X'
+        user_one = True
     show_board()
