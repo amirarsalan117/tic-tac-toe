0b14ed6cf5405ecd92734b97a56d6cae220be8f8
amirarsalan117 <122307820+amirarsalan117@users.noreply.github.com>
2023-01-18 12:38:13 +0000

    added func that find winner

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 def show_board():
     for i in range(3):
         print(board[i])
-def check_winner(arr):
-    # for i in range(3):
-    #     for j in range(3):
-    #         if True:
-    #             return True
-    # return False
-    pass
-        
 
+
+
+def check_winner(arr, user_turn):
+    for i in range(3):
+        if arr[0][i] == arr[1][i] == arr[2][i] == user_turn:
+            return True
+    else:
+        for i in range(3):
+            if arr[i][0] == arr[i][1] == arr[i][2] == user_turn:
+                return True
+        else:        
+            if arr[0][0] == arr[1][1] == arr[2][2] == user_turn:
+                return True
+            elif arr[2][0] == arr[1][1] == arr[0][2] == user_turn:
+                return True
+
+ 
+winner = ''
 board = [['-','-','-'],['-','-','-'],['-','-','-',]]
 show_board()
 user_one = True
@@ -23,3 +33,12 @@ while is_game_on:
         board[int(user_input[0])][int(user_input[1])] = 'X'
         user_one = True
     show_board()
+    if check_winner(board ,'X'):
+        winner = 'X'
+        is_game_on = False
+    elif check_winner(board , 'O'):
+        winner = 'O'
+        is_game_on = False
+    
+
+print(f'the winner is {winner}')
--- test.py
+++ test.py
@@ -1,6 +1,2 @@
-def check_winner(arr):
-    for i in range(3):
-        for j in range(3):
-            print(f'if arr{i}{j} == arr{i}{j}:')
-board = [['0','1','2'],['3','4','5'],['6','7','8']]
-print(check_winner(board))
+
+board = [['-','-','-'],['-','-','-'],['-','-','-',]]
